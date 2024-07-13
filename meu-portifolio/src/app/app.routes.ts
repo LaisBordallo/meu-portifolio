11b9e4df76ed69e8ae8055aa62db89818f8ea6ad
Laís Cristine Bordallo Pinheiro <laiscbordallo@gmail.com>
2024-07-13 19:21:36 -0300

    Mudanças

--- meu-portifolio/src/app/app.routes.ts
+++ meu-portifolio/src/app/app.routes.ts
@@ -10,3 +10,17 @@ export const routes: Routes = [
     {path: 'about', component: AboutComponent},
     {path: 'projects', component: ProjectsComponent}
 ];
+
+// import { Routes } from '@angular/router';
+// import { HomeComponent } from './home/home.component';
+// import { AboutComponent } from './about/about.component';
+// import { ProjectsComponent } from './projects/projects.component';
+
+// const routes: Routes = [
+//     {path: '', redirectTo: '/home', pathMatch: 'full'},
+//     {path: 'home', component: HomeComponent},
+//     {path: 'about', component: AboutComponent},
+//     {path: 'projects', component: ProjectsComponent}
+// ];
+
+// export const AppRoutes: Routes = [];
