f7f1bf0e370316dad0a595294ac75b2da469f1ab
Laís Cristine Bordallo Pinheiro <laiscbordallo@gmail.com>
2024-07-13 20:05:54 -0300

    Mudanças serviço da navbar.

--- meu-portifolio/src/app/navbar/navbar.component.ts
+++ meu-portifolio/src/app/navbar/navbar.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+// import { Component } from '@angular/core';
+// import { RouterModule } from '@angular/router';
+
+// @Component({
+//   selector: 'app-navbar',
+//   standalone: true,
+//   imports: [RouterModule],
+//   templateUrl: './navbar.component.html',
+//   styleUrl: './navbar.component.scss'
+// })
+// export class NavbarComponent {
+
+// }
+
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavbarService } from '../../service/navbar.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // Adicione esta importação
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule], // Adicione RouterModule aqui
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  constructor(private navbarService: NavbarService, private router: Router) {}
 
+  navegarPara(path: string, event: Event) {
+    event.preventDefault();
+    this.router.navigate([path]);
+    this.navbarService.closeNavbar();
+  }
+
+  toggleNavbar() {
+    this.navbarService.toggleNavbar();
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.navbar')) {
+      this.navbarService.closeNavbar();
+    } else if (target.closest('.nav-link')) {
+      this.navbarService.closeNavbar();
+    }
+  }
 }
