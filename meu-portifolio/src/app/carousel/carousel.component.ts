4459cc25ec49e304d0ab435d99612b228d3f0284
Laís Cristine Bordallo Pinheiro <laiscbordallo@gmail.com>
2024-07-20 10:07:51 -0300

    Carousel conflict

--- meu-portifolio/src/app/carousel/carousel.component.ts
+++ meu-portifolio/src/app/carousel/carousel.component.ts
@@ -1,42 +1,26 @@
 import { Component } from '@angular/core';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [],
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.scss'
+  styleUrls: ['./carousel.component.scss'],
+  imports: [SlickCarouselModule]
 })
 export class CarouselComponent {
-  techLogos = [
-    {scr: 'assets/img/angular.png', alt: 'Angular logo'},
-    {scr: 'assets/img/bootstrap.png', alt: 'Bootstrap logo'},
-    {scr: 'assets/img/css.png', alt: 'CSS logo'},
-    {scr: 'assets/img/html.png', alt: 'HTML logo'},
-    {scr: 'assets/img/javascript.png', alt: 'JavaScript logo'},
-    {scr: 'assets/img/typescript.png', alt: 'TypeScript logo'},
+  images = [
+    { src: 'assets/images/carousel1.jpg', alt: 'Image 1' },
+    { src: 'assets/images/carousel2.jpg', alt: 'Image 2' },
+    { src: 'assets/images/carousel3.jpg', alt: 'Image 3' },
   ];
 
-  currentSlide = 0;
-
-  get carouselTransform() {
-    return `translateX(-${this.currentSlide * 100}%)`;
-  }
-
-  nextSlide() {
-    if (this.currentSlide < this.techLogos.length - 1) {
-      this.currentSlide++;
-    } else {
-      this.currentSlide = 0; // Volta para o primeiro slide
-    }
-  }
-
-  prevSlide() {
-    if (this.currentSlide > 0) {
-      this.currentSlide--;
-    } else {
-      this.currentSlide = this.techLogos.length - 1; // Vai para o último slide
-    }
-  }
-
+  slideConfig = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  };
 }
